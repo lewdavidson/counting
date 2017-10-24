6f2b403e03a0493c934592c98dae0afbd15ee299
Lew Davidson and Nathan Barrett <nathan@>
2017-10-24 10:33:05 -0700

    tired

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+  $("form#count-form").submit(function(event){
+    event.preventDefault();
+    var countTo = parseInt($("input#count-to").val());
+    var countBy = parseInt($("input#count-by").val());
+    var userOutput = [];
+    for (var index = countBy; index <= countTo; index += countBy) {
+      userOutput.push(index);
+    }
+    if (Number.isInteger(countTo)) && (Number.isInteger(countBy)) {
+      alert(userOutput);
+    }
+    else {
+      alert("bro you trippin!");
+    }
+
+
+
+  });
+});
